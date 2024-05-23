73f6897dc470d2d9215a6998c563fab00924fcf5
cosmah <cosmahke4@gmail.com>
2024-05-23 15:41:57 +0300

    added tailind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './src/**/*.{html,js,jsx}'
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
