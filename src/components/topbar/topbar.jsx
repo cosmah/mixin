aa68ff6349702d8c1a3fa2cd324532f564a27096
cosmah <cosmahke4@gmail.com>
2024-10-24 19:40:03 +0300

    modifying the top bar

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./topbar.css"; // Ensure you have this CSS file
 import Top from "./top/top";
-import Middle from "./middle/middle";
-import Bottom from "./bottom/bottom";
+// import Middle from "./middle/middle";
+// import Bottom from "./bottom/bottom";
 
 
 function Topbar() {
   return (
     <div>
       <Top/>
-      <Middle/>
+      {/* <Middle/> */}
       {/* <Bottom/> */}
     </div>
   );
